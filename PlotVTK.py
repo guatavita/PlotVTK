0b5bb98d8edd811a0b9a4057df5bf12c3d2aa5bd
unknown <bastien.rig@gmail.com>
2022-01-24 17:49:27 +0100

    push plot vtk

---
+++ PlotVTK.py
@@ -0,0 +1,147 @@
+# Created by Bastien Rigaud at 24/01/2022
+# Bastien Rigaud, PhD
+# Laboratoire Traitement du Signal et de l'Image (LTSI), INSERM U1099
+# Campus de Beaulieu, Université de Rennes 1
+# 35042 Rennes, FRANCE
+# bastien.rigaud@univ-rennes1.fr
+# Description:
+
+import numpy as np
+import vtk
+from vtk.util import numpy_support
+
+
+def MakeAxesActor(scale, xyzLabels):
+    """
+    :param scale: Sets the scale and direction of the axes.
+    :param xyzLabels: Labels for the axes.
+    :return: The axes actor.
+    """
+    axes = vtk.vtkAxesActor()
+    axes.SetScale(scale)
+    axes.SetShaftTypeToCylinder()
+    axes.SetXAxisLabelText(xyzLabels[0])
+    axes.SetYAxisLabelText(xyzLabels[1])
+    axes.SetZAxisLabelText(xyzLabels[2])
+    axes.SetCylinderRadius(0.5 * axes.GetCylinderRadius())
+    axes.SetConeRadius(1.025 * axes.GetConeRadius())
+    axes.SetSphereRadius(1.5 * axes.GetSphereRadius())
+    tprop = axes.GetXAxisCaptionActor2D().GetCaptionTextProperty()
+    tprop.ItalicOn()
+    tprop.ShadowOn()
+    tprop.SetFontFamilyToTimes()
+    # Use the same text properties on the other two axes.
+    axes.GetYAxisCaptionActor2D().GetCaptionTextProperty().ShallowCopy(tprop)
+    axes.GetZAxisCaptionActor2D().GetCaptionTextProperty().ShallowCopy(tprop)
+    return axes
+
+
+def append_polydata(polydata_list=[]):
+    append_filter = vtk.vtkAppendPolyData()
+    i = 0
+    for poly in polydata_list:
+        array_names = [poly.GetPointData().GetArrayName(arrayid) for arrayid in
+                       range(poly.GetPointData().GetNumberOfArrays())]
+        if 'label_color' not in array_names:
+            ntype = numpy_support.get_numpy_array_type(poly.GetPoints().GetDataType())
+            label_color = numpy_support.numpy_to_vtk(i * np.ones(poly.GetNumberOfPoints(), dtype=ntype), deep=1)
+            label_color.SetName('label_color')
+            poly.GetPointData().SetScalars(label_color)
+        append_filter.AddInputData(poly)
+        i += 1
+    append_filter.Update()
+    return append_filter.GetOutput()
+
+
+# TODO add dvf with glyph
+def plot_vtk(polydata, secondary=None, opacity=.5):
+    '''
+    :param polydata: polydata or list of polydata to plot
+    :param secondary: secondary polydata or list of polydata for comparison
+    :param opacity: opacity of polydata (default: 0.5)
+    :return:
+    '''
+
+    if isinstance(polydata, list):
+        polydata = append_polydata(polydata)
+
+    if isinstance(secondary, list):
+        secondary = append_polydata(secondary)
+
+    center_filter = vtk.vtkCenterOfMass()
+    center_filter.SetInputData(polydata)
+    center_filter.SetUseScalarsAsWeights(False)
+    center_filter.Update()
+    center = center_filter.GetCenter()
+    bounds = polydata.GetBounds()
+
+    # Create camera
+    camera = vtk.vtkCamera()
+    camera.SetPosition(-5 * (abs(bounds[0] - bounds[1]) / 2), -3 * (abs(bounds[2] - bounds[3]) / 2), 0)
+    camera.SetFocalPoint(center[0], center[1], center[2])
+    camera.SetViewUp(0, 0, 1)
+
+    colors = vtk.vtkNamedColors()
+    scalar_range = polydata.GetScalarRange()
+
+    # Create axes actor
+    xyzLabels = ['X', 'Y', 'Z']
+    scale = [1.5, -1.5, 1.5]
+    axes = MakeAxesActor(scale, xyzLabels)
+    om = vtk.vtkOrientationMarkerWidget()
+    om.SetOrientationMarker(axes)
+    # Position upper left in the viewport.
+    om.SetViewport(0, 0, 0.2, 0.2)
+
+    # Create the mapper that corresponds the objects of the vtk file
+    # into graphics elements
+    mapper = vtk.vtkDataSetMapper()
+    mapper.SetInputData(polydata)
+    mapper.SetScalarRange(scalar_range)
+
+    # Create the Actor
+    actor = vtk.vtkActor()
+    actor.GetProperty().SetOpacity(opacity);
+    actor.SetMapper(mapper)
+
+    if secondary:
+        sec_mapper_ = vtk.vtkDataSetMapper()
+        sec_mapper_.SetInputData(secondary)
+        sec_mapper_.SetScalarRange(scalar_range)
+        sec_actor = vtk.vtkActor()
+        sec_actor.GetProperty().SetOpacity(opacity);
+        sec_actor.SetMapper(sec_mapper_)
+
+    # Create the Renderer
+    renderer = vtk.vtkRenderer()
+    renderer.SetBackground(1, 1, 1)  # Set background to white
+    renderer.SetActiveCamera(camera)
+
+    # Create the RendererWindow
+    renderer_window = vtk.vtkRenderWindow()
+    renderer_window.SetWindowName('PlotVTK')
+    renderer_window.AddRenderer(renderer)
+    renderer_window.SetSize(600, 600)
+
+    # Create the RendererWindowInteractor and display the vtk_file
+    interactor = vtk.vtkRenderWindowInteractor()
+    interactor.SetRenderWindow(renderer_window)
+    interactor.SetInteractorStyle(vtk.vtkInteractorStyleTrackballCamera())
+
+    om.SetInteractor(interactor)
+    om.EnabledOn()
+    om.InteractiveOff()
+
+    # Add actor to the schene
+    renderer.AddActor(actor)
+    if secondary:
+        renderer.AddActor(sec_actor)
+
+    # Render and interact
+    renderer_window.Render()
+    renderer.GetActiveCamera().Zoom(.8)
+    renderer_window.Render()
+
+    interactor.Initialize()
+    renderer_window.Render()
+    interactor.Start()
---
+++ README.md
@@ -0,0 +1,25 @@
+# PlotVTK
+
+## Table of contents
+* [General info](#general-info)
+* [Example](#example)
+* [Dependencies](#dependencies)
+
+## General info
+Bastien Rigaud, PhD
+Laboratoire Traitement du Signal et de l'Image (LTSI), INSERM U1099
+Campus de Beaulieu, Université de Rennes 1
+35042 Rennes, FRANCE
+bastien.rigaud@univ-rennes1.fr
+
+## Example 
+
+```
+```
+
+## Dependencies
+
+Run:
+```
+pip install -r requirements.txt
+```
